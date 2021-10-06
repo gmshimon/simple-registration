88c87d3e0363f4530f32be0ee1a163a07cfc27e9
GMShimon <simon.rosedale99@gmail.com>
2021-10-06 23:48:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = (e) => {
+    console.log(email, password);
+    e.preventDefault();
+  }
+  const handleEmail = e => {
+    setEmail(e.target.value);
+  }
+  const handlePassword = e => {
+    setPassword(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-5">
+      <br /><br /><br /><br />
+      <h2 className="text-primary">Please Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="row mb-3">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input onBlur={handleEmail} type="email" className="form-control" id="inputEmail3" />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input onBlur={handlePassword} type="password" className="form-control" id="inputPassword3" />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="gridCheck1" />
+              <label className="form-check-label" htmlFor="gridCheck1">
+                Example checkbox
+              </label>
+            </div>
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary">Sign in</button>
+      </form>
     </div>
   );
 }
---
+++ src/Firebase/firebase.congfig.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDYlJtk_ipoHJE2pACmWQ9htUmFk3zGVmM",
+    authDomain: "simple-firebase-auth-6a7ca.firebaseapp.com",
+    projectId: "simple-firebase-auth-6a7ca",
+    storageBucket: "simple-firebase-auth-6a7ca.appspot.com",
+    messagingSenderId: "702112547454",
+    appId: "1:702112547454:web:11e7f65a177dd9eedb8e2f"
+};
+export default firebaseConfig;
